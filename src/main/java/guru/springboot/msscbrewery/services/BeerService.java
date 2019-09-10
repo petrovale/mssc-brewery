5c79189e3d0726b46e86a6c1de29189193274568
petrovale <leha.isakov@yandex.ru>
2019-09-11 00:16:02 +0300

    Initial Project

---
+++ src/main/java/guru/springboot/msscbrewery/services/BeerService.java
@@ -0,0 +1,9 @@
+package guru.springboot.msscbrewery.services;
+
+import guru.springboot.msscbrewery.web.model.BeerDto;
+import java.util.UUID;
+
+public interface BeerService {
+
+  BeerDto getBeerById(UUID beerId);
+}
---
+++ src/main/java/guru/springboot/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,17 @@
+package guru.springboot.msscbrewery.services;
+
+import guru.springboot.msscbrewery.web.model.BeerDto;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+  @Override
+  public BeerDto getBeerById(UUID beerId) {
+    return BeerDto.builder().id(UUID.randomUUID())
+        .beerName("Galaxy Cat")
+        .beerStyle("Pale Ale")
+        .build();
+  }
+}
