749939c9781cbb286d8c794e3b5fd0d54cc9e307
petrovale <leha.isakov@yandex.ru>
2019-09-11 23:04:15 +0300

    adding request mapping and test

--- src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class BeerController {
   }
 
   @PostMapping // POST - create new beer
-  public ResponseEntity handlePost(BeerDto beerDto){
+  public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
 
     BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -41,7 +42,7 @@ public class BeerController {
   }
 
   @PutMapping({"/{beerId}"})
-  public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto){
+  public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
 
     beerService.updateBeer(beerId, beerDto);
 
