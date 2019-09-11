d3fb1fa16d0144336b33918f612aa7d794a12ffc
petrovale <leha.isakov@yandex.ru>
2019-09-11 22:19:34 +0300

    added Post Beer

--- src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
@@ -3,10 +3,12 @@ package guru.springboot.msscbrewery.web.controller;
 import guru.springboot.msscbrewery.services.BeerService;
 import guru.springboot.msscbrewery.web.model.BeerDto;
 import java.util.UUID;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,17 @@ public class BeerController {
   @GetMapping("{beerId}")
   public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
     return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+  }
+
+  @PostMapping // POST - create new beer
+  public ResponseEntity handlePost(BeerDto beerDto){
+
+    BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+    HttpHeaders headers = new HttpHeaders();
+    //todo add hostname to url
+    headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+    return new ResponseEntity(headers, HttpStatus.CREATED);
   }
 }
