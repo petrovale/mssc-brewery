81d3c91164ee7d6e59394f8eb55688f8007aad16
petrovale <leha.isakov@yandex.ru>
2019-09-12 00:19:43 +0300

    added DELETE, POST, PUT request for Customer

--- src/main/java/guru/springboot/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springboot/msscbrewery/services/CustomerServiceImpl.java
@@ -2,8 +2,10 @@ package guru.springboot.msscbrewery.services;
 
 import guru.springboot.msscbrewery.web.model.CustomerDto;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
@@ -13,5 +15,23 @@ public class CustomerServiceImpl implements CustomerService {
         .id(UUID.randomUUID())
         .name("Alex")
         .build();
+  }
+
+  @Override
+  public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+    return CustomerDto.builder()
+        .id(UUID.randomUUID())
+        .build();
+  }
+
+  @Override
+  public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+    //todo impl
+    log.debug("Updating....");
+  }
+
+  @Override
+  public void deleteById(UUID customerId) {
+    log.debug("Deleting.... ");
   }
 }
