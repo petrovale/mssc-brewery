9f9e9d34246ba05eb311511d1e3827b9338ca4a3
petrovale <leha.isakov@yandex.ru>
2019-09-11 22:45:06 +0300

    added Put Controller Beer

--- src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springboot/msscbrewery/web/controller/BeerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,13 @@ public class BeerController {
     headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
     return new ResponseEntity(headers, HttpStatus.CREATED);
+  }
+
+  @PutMapping({"/{beerId}"})
+  public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto){
+
+    beerService.updateBeer(beerId, beerDto);
+
+    return new ResponseEntity(HttpStatus.NO_CONTENT);
   }
 }
