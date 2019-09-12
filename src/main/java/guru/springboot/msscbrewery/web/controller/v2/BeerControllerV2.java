12ff48039304e7ee20b3a5b79a86455e7037342c
petrovale <leha.isakov@yandex.ru>
2019-09-12 21:33:17 +0300

    adding v2 of beer controller

--- src/main/java/guru/springboot/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springboot/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -1,7 +1,7 @@
-package guru.springboot.msscbrewery.web.controller;
+package guru.springboot.msscbrewery.web.controller.v2;
 
-import guru.springboot.msscbrewery.services.BeerService;
-import guru.springboot.msscbrewery.web.model.BeerDto;
+import guru.springboot.msscbrewery.services.v2.BeerServiceV2;
+import guru.springboot.msscbrewery.web.model.v2.BeerDtoV2;
 import java.util.UUID;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,31 +11,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Deprecated
-@RequestMapping("/api/v1/beer")
+@RequestMapping("/api/v2/beer")
 @RestController
-public class BeerController {
+public class BeerControllerV2 {
+  private final BeerServiceV2 beerServiceV2;
 
-  private final BeerService beerService;
-
-  public BeerController(BeerService beerService) {
-    this.beerService = beerService;
+  public BeerControllerV2(BeerServiceV2 beerServiceV2) {
+    this.beerServiceV2 = beerServiceV2;
   }
 
-  @GetMapping("{beerId}")
-  public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
-    return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+  @GetMapping({"/{beerId}"})
+  public ResponseEntity<BeerDtoV2> getBeer(@PathVariable("beerId") UUID beerId){
+
+    return new ResponseEntity<>(beerServiceV2.getBeerById(beerId), HttpStatus.OK);
   }
 
   @PostMapping // POST - create new beer
-  public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
+  public ResponseEntity handlePost(BeerDtoV2 beerDto){
 
-    BeerDto savedDto = beerService.saveNewBeer(beerDto);
+    BeerDtoV2 savedDto = beerServiceV2.saveNewBeer(beerDto);
 
     HttpHeaders headers = new HttpHeaders();
     //todo add hostname to url
@@ -45,9 +43,9 @@ public class BeerController {
   }
 
   @PutMapping({"/{beerId}"})
-  public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+  public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDtoV2 beerDto){
 
-    beerService.updateBeer(beerId, beerDto);
+    beerServiceV2.updateBeer(beerId, beerDto);
 
     return new ResponseEntity(HttpStatus.NO_CONTENT);
   }
@@ -55,6 +53,6 @@ public class BeerController {
   @DeleteMapping({"/{beerId}"})
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteBeer(@PathVariable("beerId") UUID beerId){
-    beerService.deleteById(beerId);
+    beerServiceV2.deleteById(beerId);
   }
 }
---
+++ src/main/java/guru/springboot/msscbrewery/web/model/v2/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package guru.springboot.msscbrewery.web.model.v2;
+
+public enum BeerStyleEnum {
+
+  LAGER, PISLSNER, ALE, STOUT, GOSE, IPA
+}
