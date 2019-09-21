6a6b12c97f89f0362ab868e817f18a624c471adf
petrovale <leha.isakov@yandex.ru>
2019-09-21 22:40:01 +0300

    adding mapstruct customer

---
+++ src/main/java/guru/springboot/msscbrewery/domain/Customer.java
@@ -0,0 +1,16 @@
+package guru.springboot.msscbrewery.domain;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+  private UUID id;
+  private String name;
+}
